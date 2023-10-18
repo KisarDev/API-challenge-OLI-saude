70487cc901a648b112c5ca6c0a2d5bfe837c5ab9
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 15:45:55 -0300

    ORM migrations

--- api/models.py
+++ api/models.py
@@ -9,7 +9,7 @@ class Client(models.Model):
     name = models.CharField(max_length=50)
     birthday = models.DateField()
     sex = models.CharField(max_length=1)
-    problem_health = models.ManyToManyField(ProblemsHealth, on_delete=models.CASCADE)
+    problem_health = models.ManyToManyField(ProblemsHealth)
     date_create = models.DateTimeField(default=timezone.now)
     data_update = models.DateTimeField(default=timezone.now)
 
