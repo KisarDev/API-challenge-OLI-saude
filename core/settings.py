a0102a562f6890c6bb457b327f8836cf6781db0d
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 15:36:47 -0300

    configurating settings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #libs
+    'rest_framework',
+    'drf_yasg',
+    #apps
+    'api',
 ]
 
 MIDDLEWARE = [
