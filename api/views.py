c6024db3e4a06f332ce0e41b1014cf91a6dc389c
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 15:40:43 -0300

    add serializers, and setup views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import ClientSerializer
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.exceptions import APIException
 
-# Create your views here.
+@swagger_auto_schema(method='post', request_body=ClientSerializer)
+@api_view(['POST'])
+def criar(request):
+    """
+    Exemplo da requisição
+    """
+    if request.method == 'GET':
+        raise APIException(403, 'Sem acesso get')
+    
+    serializer = ClientSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
