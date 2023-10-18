4d0cb634b92e929f391299a150f8027b5c76987f
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 15:59:56 -0300

    test 1 modeling many to many

--- api/models.py
+++ api/models.py
@@ -4,12 +4,15 @@ from django.utils import timezone
 class ProblemsHealth(models.Model):
     name_problem = models.CharField(max_length=30)
     rating = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name_problem
     
 class Client(models.Model):
     name = models.CharField(max_length=50)
     birthday = models.DateField()
     sex = models.CharField(max_length=1)
-    problem_health = models.ManyToManyField(ProblemsHealth)
+    problem_health = models.ManyToManyField(ProblemsHealth, unique=False)
     date_create = models.DateTimeField(default=timezone.now)
     data_update = models.DateTimeField(default=timezone.now)
 
--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 
 from api.models import ProblemsHealth, Client
 
+class ProblemsHealthSerializer(serializers.ModelSerializer):
+    model = ProblemsHealth
+    fields = '__all__'
+
+        
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = '__all__'
-
-        
