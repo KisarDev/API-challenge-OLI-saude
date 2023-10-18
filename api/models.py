717557cb827b1f1ce2ddcfc1771a24cc7c164d9b
Cesar Augusto <cesarmartins.pro@gmail.com>
2023-10-18 15:44:29 -0300

    modeling and serializing clients & problem_health

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ProblemsHealth(models.Model):
+    name_problem = models.CharField(max_length=30)
+    rating = models.IntegerField()
+    
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    sex = models.CharField(max_length=1)
+    problem_health = models.ManyToManyField(ProblemsHealth, on_delete=models.CASCADE)
+    date_create = models.DateTimeField(default=timezone.now)
+    data_update = models.DateTimeField(default=timezone.now)
+
+
+
---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from api.models import ProblemsHealth, Client
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+        
