74508955d8329fb301c9f4f2efe982861c510554
unity-team-developers <cesarmartins.pro@gmail.com>
2023-10-24 20:32:11 +0100

    Swagger infos

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,10 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Sua API Django REST",
+      title="Desafio de Desenvolvedor Backend - Oli Saúde",
       default_version='v1',
-      description="Descrição da sua API",
-      terms_of_service="https://www.suaapi.com/terms/",
-      contact=openapi.Contact(email="contato@suaapi.com"),
-      license=openapi.License(name="Sua Licença"),
+      description="O Desafio consistia em criar uma API capaz de gerenciar informações de clientes de saúde, fornecendo recursos para criar, editar, visualizar e listar clientes. Além disso, a API precisava calcular o risco de saúde com base nos problemas de saúde relatados pelos clientes, usando a fórmula score = (1 / (1 + e^(-2.8 + sd))) * 100. Esse calculo é baseado em um atributo cujo é o grau da gravidade do problema de saúde do cliente.",
+      contact=openapi.Contact(email="cesarmartins.pro@gmail.com"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
